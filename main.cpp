15f15c36147b6a9dd8fe3058b5205b5cea6a48ad
Michael Browell <mbrowell1984@gmail.com>
2016-05-03 11:09:16 -0700

    Variables
    C++/Game Tutorial 2: Variables

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, world.\n";
+    float playerDamage = 100.0f;
+    float zombieHealth = 200.0f;
+
+    zombieHealth = zombieHealth - playerDamage;
+
+    cout << "The zombie has " << zombieHealth << " health left.\n";
 
     return 0;
 }
